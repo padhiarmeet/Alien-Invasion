582a2db0d6860fb27699417adf37e1f5049dc66b
padhiarmeet <padhiarmeet00@gmail.com>
2024-12-10 22:35:37 +0530

    10/12/2024

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+
+    def __init__(self):
+        #Screen settings are here.....
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (61,64,91)
